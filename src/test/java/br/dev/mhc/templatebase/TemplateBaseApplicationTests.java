303cc6595079f0e93c94f220933c307b8ef9e9b3
Marcelo Henrique Cezario <marcelocezario@gmail.com>
2024-04-10 19:25:02 -0300

    Initial commit

---
+++ src/test/java/br/dev/mhc/templatebase/TemplateBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.mhc.templatebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
